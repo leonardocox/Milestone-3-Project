512df1ca09d7df385adf25d617fc7cc99a0887c5
leonardocox <leonardocx0@gmail.com>
2022-12-12 16:33:40 -0500

    Setting up the home page and getting this disco ready with proper linking

--- project_music/frontend/src/components/HomePage.js
+++ project_music/frontend/src/components/HomePage.js
@@ -3,10 +3,33 @@ import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
 import Room from "./Room";
 import { BrowserRouter, Routes, Route, Link, Redirect } from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+  }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            Disco Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Disco!
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Disco?
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
   }
 
   render() {
