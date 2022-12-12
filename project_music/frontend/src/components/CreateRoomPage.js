4da08eaa1558a8bb152c81791e2e736a561b9c1b
leonardocox <leonardocx0@gmail.com>
2022-12-12 11:30:34 -0500

    Adding buttons and other componenets to form for making a room

--- project_music/frontend/src/components/CreateRoomPage.js
+++ project_music/frontend/src/components/CreateRoomPage.js
@@ -32,12 +32,46 @@ export default class CreateRoomPage extends Component {
               <div align="center">Guest Control of Playback</div>
             </FormHelperText>
             <RadioGroup row defaultValue="true">
-              <FormLabelControl
+              <FormControlLabel
                 value="true"
                 control={<Radio color="primary" />}
+                label="Play/Pause"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="false"
+                control={<Radio color="secondary" />}
+                label="No control"
+                labelPlacement="bottom"
               />
             </RadioGroup>
           </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type="number"
+              defaultValue={this.defaultVotes}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
+            />
+            <FormHelperText>
+              <div align="center">Votes Required to Skip Song</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained">
+            Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="primary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
         </Grid>
       </Grid>
     );
