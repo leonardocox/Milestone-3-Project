faa90e15cd2babf2a47ccbc23803609a399833b8
leonardocox <leonardocx0@gmail.com>
2022-12-12 15:48:36 -0500

    centering and also creating the room join page

--- project_music/frontend/src/components/RoomJoinPage.js
+++ project_music/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,37 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
+import { Link } from "react-router-dom";
+import { TextField, Button, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-export default class RoomJoinPage extends Component {
-  constructor(props) {
-    super(props);
-  }
+export default function RoomJoinPage(props) {
+  const navigate = useNavigate();
 
-  render() {
-    return <p>Welcome to the room join page</p>;
-  }
+  const [roomCode, setroomCode] = useState("");
+  const [error, seterror] = useState("");
+
+  const handleTextFieldChange = () => {
+    setroomCode(event.target.value);
+  };
+
+  const roomButtonPressed = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: roomCode,
+      }),
+    };
+
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          navigate(`/room/${roomCode}`);
+        } else {
+          seterror("Room not found.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 }
