14b91dc3cc40e926ae7ca4f8067074e4b0e67c58
leonardocox <leonardocx0@gmail.com>
2022-12-12 17:59:24 -0500

    Now if you already created a room you will be redirected to the room and not the homepage

--- project_music/frontend/src/components/HomePage.js
+++ project_music/frontend/src/components/HomePage.js
@@ -2,47 +2,71 @@ import React, { Component } from "react";
 import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
 import Room from "./Room";
-import { BrowserRouter, Routes, Route, Link, Redirect } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+  Navigate,
+} from "react-router-dom";
 import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
   }
 
-  // componentdidmount means that the component just rendered on the screen for the first time
-  async componentDidMount() {}
+  // Basically is a going to run right as the page loads
+
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
+  }
 
   renderHomePage() {
-    return (
-      <Grid container spacing={3}>
-        <Grid item xs={12} align="center">
-          <Typography variant="h3" compact="h3">
-            Disco Room
-          </Typography>
+    if (this.state.roomCode) {
+      return <Navigate to={`/room/${this.state.roomCode}`} replace={true} />;
+    } else {
+      return (
+        <Grid container spacing={3}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h3" compact="h3">
+              Disco Room
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button color="primary" to="/join" component={Link}>
+                Join a Disco
+              </Button>
+              <Button color="secondary" to="/create" component={Link}>
+                Create a Disco
+              </Button>
+            </ButtonGroup>
+          </Grid>
         </Grid>
-        <Grid item xs={12} align="center">
-          <ButtonGroup disableElevation variant="contained" color="primary">
-            <Button color="primary" to="/join" component={Link}>
-              Join a Disco!
-            </Button>
-            <Button color="secondary" to="/create" component={Link}>
-              Create a Disco?
-            </Button>
-          </ButtonGroup>
-        </Grid>
-      </Grid>
-    );
+      );
+    }
   }
 
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={this.renderHomePage()}></Route>
+          <Route exact path="/" element={this.renderHomePage()} />
           <Route path="/join" element={<RoomJoinPage />} />
           <Route path="/create" element={<CreateRoomPage />} />
-          <Route path="/room/:roomCode" element={<Room />} />
+          <Route path="/room/:roomCode" element={<Room />} />{" "}
+          {/* : in path value represents a parameter in url */}
         </Routes>
       </BrowserRouter>
     );
