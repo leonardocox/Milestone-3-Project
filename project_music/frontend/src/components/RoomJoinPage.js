dd7805806f528aba02fa38836f361e68e6faaa32
leonardocox <leonardocx0@gmail.com>
2022-12-12 15:49:44 -0500

    Added overall layout of the join room page

--- project_music/frontend/src/components/RoomJoinPage.js
+++ project_music/frontend/src/components/RoomJoinPage.js
@@ -34,4 +34,34 @@ export default function RoomJoinPage(props) {
         console.log(error);
       });
   };
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <TextField
+          error={error}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          helperText={error}
+          variant="outlined"
+          onChange={handleTextFieldChange}
+        />
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color="primary" onClick={roomButtonPressed}>
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color="secondary" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
