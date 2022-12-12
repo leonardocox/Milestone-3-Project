e5a0a346efeb36bb842f4ecbc94f14e3f0bde971
leonardocox <leonardocx0@gmail.com>
2022-12-12 11:06:49 -0500

    Importing and setting up some material UI components

--- project_music/frontend/src/components/CreateRoomPage.js
+++ project_music/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
+import {
+  Button,
+  Grid,
+  Typography,
+  TextField,
+  FormHelperText,
+  FormControl,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+} from "@mui/material";
 
 export default class CreateRoomPage extends Component {
+  defaultVotes = 2;
+
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <p>Welcome to the create room page</p>;
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+            Create a Disco!
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl component="fieldset">
+            <FormHelperText>
+              <div align="center">Guest Control of Playback</div>
+            </FormHelperText>
+            <RadioGroup row defaultValue="true">
+              <FormLabelControl
+                value="true"
+                control={<Radio color="primary" />}
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+      </Grid>
+    );
   }
 }
